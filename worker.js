d907f683c13e77f1fa605c773e6fa6b6b2ac84d1
createxdream <88586743+createxdream@users.noreply.github.com>
2025-06-13 11:47:08 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1720,7 +1720,7 @@ async exportConfig(request, env, ctx) {
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
               </svg>
-              添加链接
+              申请链接
             </button>
             
             <a href="https://obai.cc/" target="_blank" class="mt-4 flex items-center px-4 py-2 text-gray-600 hover:text-primary-500 transition duration-300">
